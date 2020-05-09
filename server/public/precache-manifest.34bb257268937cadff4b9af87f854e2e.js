7f7cc4349cde659986b594bd1a8dc2cbb36e4b65
Udbhav <udbhavs03@gmail.com>
2020-05-09 17:15:01 +0530

    add public dir

---
+++ server/public/precache-manifest.34bb257268937cadff4b9af87f854e2e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3205f8736a434916e821",
+    "url": "/css/app.51a193ae.css"
+  },
+  {
+    "revision": "c6703a81a01a4f7ad02e",
+    "url": "/css/connectionEditor~connectionList~graphView~item~itemList.1aa6f2e9.css"
+  },
+  {
+    "revision": "c89a71ffd7ba65d201da",
+    "url": "/css/connectionEditor~itemEditor.a2435f9a.css"
+  },
+  {
+    "revision": "57948c1f0376edfa8abc",
+    "url": "/css/connectionList.60de6665.css"
+  },
+  {
+    "revision": "fe666b0b48c5d214dbde",
+    "url": "/css/graphView.c98ae89f.css"
+  },
+  {
+    "revision": "5370bf101942c8160bf5",
+    "url": "/css/item.64b1d33c.css"
+  },
+  {
+    "revision": "00c4320ae189e8c6defd",
+    "url": "/css/itemEditor.1a3f1748.css"
+  },
+  {
+    "revision": "3021113d15dc7df6bc22",
+    "url": "/css/itemList.c295f418.css"
+  },
+  {
+    "revision": "7ecb06c69959a6cb9125",
+    "url": "/css/workspaceSettings.a80ac1d6.css"
+  },
+  {
+    "revision": "763db88243891ede292d52457fcf5780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9000d122ddee120822c5",
+    "url": "/js/about.af0f2154.js"
+  },
+  {
+    "revision": "3205f8736a434916e821",
+    "url": "/js/app.431d9e91.js"
+  },
+  {
+    "revision": "efff55ff0572740bd5f6",
+    "url": "/js/chunk-vendors.f599f94a.js"
+  },
+  {
+    "revision": "93797c803a94799ea850",
+    "url": "/js/connectionEditor.3f09a565.js"
+  },
+  {
+    "revision": "c6703a81a01a4f7ad02e",
+    "url": "/js/connectionEditor~connectionList~graphView~item~itemList.83eeb35d.js"
+  },
+  {
+    "revision": "c89a71ffd7ba65d201da",
+    "url": "/js/connectionEditor~itemEditor.699ee625.js"
+  },
+  {
+    "revision": "57948c1f0376edfa8abc",
+    "url": "/js/connectionList.194eb962.js"
+  },
+  {
+    "revision": "0a52617c6c0b6064569a",
+    "url": "/js/createWorkspace.fb2c4777.js"
+  },
+  {
+    "revision": "dd22572c12b763769bbb",
+    "url": "/js/forbidden.123ae4f9.js"
+  },
+  {
+    "revision": "fe666b0b48c5d214dbde",
+    "url": "/js/graphView.8c8b8fa9.js"
+  },
+  {
+    "revision": "5370bf101942c8160bf5",
+    "url": "/js/item.ff7b65f2.js"
+  },
+  {
+    "revision": "00c4320ae189e8c6defd",
+    "url": "/js/itemEditor.e30098c9.js"
+  },
+  {
+    "revision": "3021113d15dc7df6bc22",
+    "url": "/js/itemList.40e59e07.js"
+  },
+  {
+    "revision": "aa9ba6a3121036197422",
+    "url": "/js/login.7b19b41e.js"
+  },
+  {
+    "revision": "6500f9ea76c64d356208",
+    "url": "/js/register.9b4c0e5e.js"
+  },
+  {
+    "revision": "d1919611c53a67638514",
+    "url": "/js/workspace.3a5fa4e8.js"
+  },
+  {
+    "revision": "7ecb06c69959a6cb9125",
+    "url": "/js/workspaceSettings.1712b0d1.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
